7e781004c024d9ae50956f2bd37517f599207dce
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-07-03 22:33:48 +0100

    Refactor code

--- src/env.mjs
+++ src/env.mjs
@@ -3,15 +3,23 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
+    // Most .env variabes should go here by default
+
     NODE_ENV: z.enum(["development", "test", "production"]),
+    DATABASE_URL: z.string().url(),
     CLERK_SECRET_KEY: z.string().min(1),
   },
   client: {
+    // Client .env variables go here
+    // usually client .env variables are denoted
+    // with a framework specific prefix
+    // in this case that's `NEXT_PUBLIC`
+
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
   },
-  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
+    // destructure all .env variables used above here
+    // this is needed because of a bug in nextJS caching
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
@@ -20,8 +28,4 @@ export const env = createEnv({
   },
 
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  // For Next.js >= 13.4.4, you only need to destructure client variables:
-  // experimental__runtimeEnv: {
-  //   NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
-  // }
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,16 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  // by default all pages are set to private by default by the clerk middleware
+  // for now, my application doesn't have any private pages
+  // so the following line sets them all back to being public pages
+
+  publicRoutes: ["/(.*)"],
+});
 
 export const config = {
+  // Disable the middleware on the following pages
+  // used for static routes and generated files
+
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
