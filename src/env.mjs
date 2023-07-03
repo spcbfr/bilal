a5cb6301793c5b189cc41b4d7ebb91347a34e71e
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-07-03 21:03:34 +0100

    Remove additional key

--- src/env.mjs
+++ src/env.mjs
@@ -9,7 +9,6 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
-    CLERK_SECRET_KEY: z.string().url().min(1),
   },
 
   /**
@@ -28,7 +27,6 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
