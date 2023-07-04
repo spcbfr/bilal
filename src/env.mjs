d754940aa82f43197a472c610c927d0577a6ad26
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-07-04 17:01:58 +0100

    Make comment style consistent

--- src/env.mjs
+++ src/env.mjs
@@ -1,25 +1,30 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
-
+/** @see https://env.t3.gg/docs/nextjs */
 export const env = createEnv({
-  server: {
-    // Most .env variabes should go here by default
+  /** Most .env variabes should go here by default  */
 
+  server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
     DATABASE_URL: z.string().url(),
     CLERK_SECRET_KEY: z.string().min(1),
   },
-  client: {
-    // Client .env variables go here
-    // usually client .env variables are denoted
-    // with a framework specific prefix
-    // in this case that's `NEXT_PUBLIC`
+  /**
+   * Client .env variables go here
+   * usually client .env variables are denoted
+   * with a framework specific prefix
+   *  in this case that's `NEXT_PUBLIC`
+   */
 
+  client: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
   },
+  /**
+   * destructure all .env variables used above here
+   * this is needed because of a bug in nextJS caching
+   */
+
   runtimeEnv: {
-    // destructure all .env variables used above here
-    // this is needed because of a bug in nextJS caching
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  // by default all pages are set to private by default by the clerk middleware
-  // for now, my application doesn't have any private pages
-  // so the following line sets them all back to being public pages
+/**
+ * by default all pages are set to private by default by the clerk middleware
+ * for now, my application doesn't have any private pages
+ * so the following line sets them all back to being public pages
+ */
 
+export default authMiddleware({
   publicRoutes: ["/(.*)"],
 });
 
-export const config = {
-  // Disable the middleware on the following pages
-  // used for static routes and generated files
+/**
+ * Disable the middleware on the following pages
+ * used for static routes and generated files
+ * @see https://clerk.com/docs/nextjs/middleware
+ */
 
+export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
