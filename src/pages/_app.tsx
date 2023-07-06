9d4cfe2a029d36de96a450d75c9b443a20544436
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-07-07 00:24:31 +0100

    feat: add shadcn/ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Nunito } from "next/font/google";
 import "~/styles/globals.css";
+
+const mainFont = Nunito({
+  subsets: ["latin"],
+  variable: "--font-main",
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider>
-      <Component {...pageProps} />
+      <main className={`${mainFont.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
     </ClerkProvider>
   );
 };
